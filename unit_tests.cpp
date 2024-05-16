135f2c552e8e32647cc8a3c89903cf349ee48dca
d13d1d21d <81983329+aspdfiionnf@users.noreply.github.com>
2024-05-16 21:35:35 +0300

    Юнит-тесты
    Добавлены юнит-тесты модулей

---
+++ unit_tests.cpp
@@ -0,0 +1,41 @@
+#include "CppUnitTest.h"
+#include "keygen.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(KeygenTests) {
+public:
+    // Тестирование generateControlSum
+    TEST_METHOD(GenerateControlSum_ZeroInput) {
+        Assert::AreEqual(keygen::generateControlSum(15), 4);
+    }
+
+    TEST_METHOD(GenerateControlSum_PositiveInput) {
+        Assert::AreEqual(keygen::generateControlSum(0), -1);
+    }
+
+    // Тестирование nextKey
+    TEST_METHOD(NextKey_PositiveInput) {
+        Assert::AreEqual(keygen::nextKey(10), 12);
+    }
+
+    TEST_METHOD(NextKey_ZeroInput) {
+        Assert::AreEqual(keygen::nextKey(0), -1);
+    }
+};
+
+TEST_CLASS(InputUtilsTests)
+{
+public:
+    // Тестирование isNumber
+    TEST_METHOD(IsNumber_NumberStrings) {
+        Assert::IsTrue(input_utils::isNumber("123"));
+        Assert::IsTrue(input_utils::isNumber("0"));
+    }
+
+    TEST_METHOD(IsNumber_NonNumberStrings) {
+        Assert::IsFalse(input_utils::isNumber(""));
+        Assert::IsFalse(input_utils::isNumber("abc"));
+        Assert::IsFalse(input_utils::isNumber("12a3"));
+    }
+};
